951f9197d684b37c06dee21bf5f9b7696ae753cd
Jose Lopez <jose.lopez95@gmail.com>
2023-04-07 01:32:34 -0400

    fixed MUI bug

--- client/src/components/Home/CategoryCard/CategoryCard.js
+++ client/src/components/Home/CategoryCard/CategoryCard.js
@@ -1,14 +1,20 @@
-import './CategoryCard.scss'
-import { Button } from '@mui/material'
-import { menus, productCategory } from '../../../utils/enum'
-const CategoryCard = ({category, setCategory, setMenu}) => {
+import "./CategoryCard.scss"
+import { Button } from "@mui/material"
+import { menus, productCategory } from "../../../utils/enum"
+const CategoryCard = ({ category, setCategory, setMenu }) => {
   return (
-    <div className='CategoryCard-main-div' onClick={() => {
+    <Button
+    sx={{
+        color:'inherit'
+    }}
+      className="CategoryCard-main-div"
+      onClick={() => {
         setCategory(category)
         setMenu(menus.showcase)
-    }}>
-        <p> The "{category}" Category</p>
-    </div>
+      }}
+    >
+      <p> The "{category}" Category</p>
+    </Button>
   )
 }
 
