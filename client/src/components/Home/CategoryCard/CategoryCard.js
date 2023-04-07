f8f5b0e086a5fc130512fd328cb7d24fab7c3b08
Jose Lopez <jose.lopez95@gmail.com>
2023-04-07 01:35:19 -0400

    fixed MUI bug

--- client/src/components/Home/CategoryCard/CategoryCard.js
+++ client/src/components/Home/CategoryCard/CategoryCard.js
@@ -5,7 +5,10 @@ const CategoryCard = ({ category, setCategory, setMenu }) => {
   return (
     <Button
     sx={{
-        color:'inherit'
+        color:'inherit',
+        alignItems: 'flex-end',
+        mt:'10px',
+        mb:'10px'
     }}
       className="CategoryCard-main-div"
       onClick={() => {
