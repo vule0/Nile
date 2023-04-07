58e1e51a131cad73ebe555e020c560c220d30f33
Jose Lopez <jose.lopez95@gmail.com>
2023-04-07 01:30:36 -0400

    fixed MUI bug

--- client/src/components/Home/CategoryCard/CategoryCard.js
+++ client/src/components/Home/CategoryCard/CategoryCard.js
@@ -3,12 +3,12 @@ import { Button } from '@mui/material'
 import { menus, productCategory } from '../../../utils/enum'
 const CategoryCard = ({category, setCategory, setMenu}) => {
   return (
-    <Button className='CategoryCard-main-div' onClick={() => {
+    <div className='CategoryCard-main-div' onClick={() => {
         setCategory(category)
         setMenu(menus.showcase)
     }}>
         <p> The "{category}" Category</p>
-    </Button>
+    </div>
   )
 }
 
